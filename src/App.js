5a880c0525508a85d80fca886a6f4952d2109806
Atick <aitmatov2005@gmail.com>
2023-09-30 19:51:49 +0600

    New commit Lesson 7

--- src/App.js
+++ src/App.js
@@ -1,72 +1,25 @@
-import { useState, useEffect } from "react";
 import React from "react";
-import AddPost from "./components/addPost";
-import Post from "./components/post";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import About from "./pages/about";
+import Users from "./pages/users";
+import Home from "./pages/home";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  const fetchPosts = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts?_limit=4"
-    );
-    const data = await response.json();
-    setPosts(data);
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  const addPost = async (title, body) => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        title: title,
-        body: body,
-        userId: Math.random().toString(36).slice(2),
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
-    const data = await response.json();
-    setPosts((prevPosts) => [data, ...prevPosts]);
-  };
-
-  const deletePost = async (id) => {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts/${id}`,
-      {
-        method: "DELETE",
-      }
-    );
-    if (response.status === 200) {
-      setPosts(
-        posts.filter((post) => {
-          return post.id !== id;
-        })
-      );
-    }
-  };
-
   return (
-    <main>
-      <h1>Consuming REST api tutorial</h1>
-      <AddPost addPost={addPost} />
-      <section className="posts-container">
-        <h2>Posts</h2>
-        {posts.map((post) => (
-          <Post
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            body={post.body}
-            deletePost={deletePost}
-          />
-        ))}
-      </section>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="users" element={<Users />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
